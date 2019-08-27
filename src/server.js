76197167e2d25d8fee94454ccb1328187c70263a
mintmarshmallow <nopp054@naver.com>
2019-08-27 18:51:29 +0900

    add a nodamailer

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
-require("dotenv").config
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { GraphQLServer } from "graphql-yoga";
-import logger from "morgan"
-import schema from "./schema"
+import logger from "morgan";
+import schema from "./schema";
+import { sendSecretMail } from "./utils";
 
-const PORT = process.env.PORT || 4000
+sendSecretMail("helloleejy20@gmail.com", "123");
 
+const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
 
-server.express.use(logger("dev"))
+server.express.use(logger("dev"));
 
-server.start({ port: PORT }, () => console.log( `Server running on port http://localhost:${PORT}`))
+server.start({ port: PORT }, () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
